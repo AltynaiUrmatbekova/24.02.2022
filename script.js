78e7e4340621e506dc49ea9962fad9cf1b529d8b
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-24 12:57:07 +0600

    changes

---
+++ script.js
@@ -0,0 +1,8 @@
+const articles = [
+    {
+    id: "article",
+    h1: "Kyrgyzstan",
+    image: "./photo.jpg",
+    body: "   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia porro provident necessitatibus modi vero sequi aspernatur, aliquam exercitationem, ex impedit labore id natus mollitia tenetur quis nostrum! Explicabo, optio inventore."
+},
+];
